04bdab2901acfe4fd6b30d66b563b4c62a00cf8b
Mark Halls <fireraisr@gmail.com>
2017-09-23 23:56:57 -0700

    able to grab hls and parse M3U file

--- build/twitch/audio.js
+++ build/twitch/audio.js
@@ -1,22 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
-const channel = "disguisedtoasths";
+const m3u8Parser = require("m3u8-parser");
+const channel = "joshog";
 const clientId = "r9oouhlom01l7aauf8n1mt89pnzbpp";
 axios_1.default
     .get(`https://api.twitch.tv/api/channels/${channel}/access_token?client_id=${clientId}`)
     .then(res => {
     const uriToken = encodeURIComponent(res.data.token);
-    console.log(`https://usher.ttvnw.net/api/channel/hls/${channel}.m3u8?token=${uriToken}&sig=${res
+    return axios_1.default.get(`https://usher.ttvnw.net/api/channel/hls/${channel}.m3u8?token=${uriToken}&sig=${res
         .data.sig}&`);
-    axios_1.default
-        .get(`https://usher.ttvnw.net/api/channel/hls/${channel}.m3u8?token=${uriToken}&sig=${res
-        .data.sig}&`)
-        .then(res => console.log(res.data))
-        .catch(err => {
-        if (err.response.status === 404) {
-            console.log("Oh Noes, I haz 404");
-        }
-    });
+})
+    .then(res => {
+    console.log(res.data);
+    let parser = new m3u8Parser.Parser();
+    parser.push(res.data);
+    parser.end();
+    const parsedManifest = parser.manifest;
+    console.log(parsedManifest);
+})
+    .catch(err => {
+    console.error(err);
 });
 //# sourceMappingURL=audio.js.map
--- src/twitch/audio.ts
+++ src/twitch/audio.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import * as m3u8Parser from "m3u8-parser";
 
-const channel = "disguisedtoasths";
+const channel = "joshog";
 const clientId = "r9oouhlom01l7aauf8n1mt89pnzbpp";
 axios
   .get(
@@ -8,20 +9,24 @@ axios
   )
   .then(res => {
     const uriToken = encodeURIComponent(res.data.token);
-    // console.log(res.data, "\n", uriToken);
-    console.log(
+    return axios.get(
       `https://usher.ttvnw.net/api/channel/hls/${channel}.m3u8?token=${uriToken}&sig=${res
         .data.sig}&`
     );
-    axios
-      .get(
-        `https://usher.ttvnw.net/api/channel/hls/${channel}.m3u8?token=${uriToken}&sig=${res
-          .data.sig}&`
-      )
-      .then(res => console.log(res.data))
-      .catch(err => {
-        if (err.response.status === 404) {
-          console.log("Oh Noes, I haz 404");
-        }
-      });
+  })
+  .then(res => {
+    console.log(res.data);
+
+    let parser = new m3u8Parser.Parser();
+    parser.push(res.data);
+    parser.end();
+
+    const parsedManifest = parser.manifest;
+    console.log(parsedManifest);
+  })
+  .catch(err => {
+    console.error(err);
+    // if (err.response.status === 404) {
+    //   console.log("Oh Noes, I haz 404");
+    // }
   });
