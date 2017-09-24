bba6d8fe8287efcaee7c318be87cac62122921de
Mark Halls <fireraisr@gmail.com>
2017-09-24 00:24:13 -0700

    removed unecessary ignores

--- build/discord/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var client_1 = require("./client");
-exports.discordClient = client_1.discordClient;
-//# sourceMappingURL=index.js.map
--- build/twitch/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var client_1 = require("./client");
-exports.twitchClient = client_1.twitchClient;
-//# sourceMappingURL=index.js.map
