37e8dd252e019041864597e11f75a34f5a797776
Mark Halls <fireraisr@gmail.com>
2017-09-30 17:11:01 -0700

    Refactor switch to if

--- src/app.ts
+++ src/app.ts
@@ -8,32 +8,33 @@ import * as botconfig from "./common/config/botconfig.json";
 
 const args = minimist(process.argv.slice(2));
 
-switch (true) {
-  case args.length > 2:
-    console.error(`Error: Too many arguments!`);
-  case args.h:
-  case args.help:
-    const options = {
-      "-c": "Update botname and oauth tokens",
-      "-k --keygen":
-        "Generate encryption keys for securing oauth in the database",
-      "-f --file": "Location of encryption key file",
-      "-u --update-password": "Update password",
-      "-d --delete": "Delete database (forgotten password)"
-    };
-    console.log(`usage: ${path.basename(__filename)} [-k]\n`);
-    console.log(columnify(options, { columns: ["Options:", "Description"] }));
-    break;
+const usage = () => {
+  const options = {
+    "-c": "Update botname and oauth tokens",
+    "-d --delete": "Delete database (forgotten password)",
+    "-f --file": "Location of encryption key file",
+    "-k --keygen":
+      "Generate encryption keys for securing oauth in the database",
+    "-u --update-password": "Update password"
+  };
+  console.log(`usage: ${path.basename(__filename)} [-k]\n`);
+  console.log(columnify(options, { columns: ["Options:", "Description"] }));
+};
 
-  case args.v:
-    console.log(`version: ${process.env.npm_package_version}`);
-    break;
-
-  default:
-    const twitch = twitchClient(botconfig);
-    const discord = discordClient(botconfig);
-    twitch.connect();
-    discord.connect();
-    twitch.setListeners(discord);
-    discord.setListeners(twitch);
+if (args.length > 2) {
+  console.error(`Error: Too many arguments!`);
+  usage();
+}
+if (args.h || args.help) {
+  usage();
+}
+if (args.v) {
+  console.log(`version: ${process.env.npm_package_version}`);
+} else {
+  const twitch = twitchClient(botconfig);
+  const discord = discordClient(botconfig);
+  twitch.connect();
+  discord.connect();
+  twitch.setListeners(discord);
+  discord.setListeners(twitch);
 }
